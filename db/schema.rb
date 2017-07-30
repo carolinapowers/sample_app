a04b7846d080b5e20dad3b1ce7fbcae546b3e0f5
carolinapowers <carolinapoloni@gmail.com>
2017-07-30 17:45:41 -0400

    Add test for the email downcasing. It uses the reload method for reloading a value from the database. Modify the before_save callback to mutate the object itself with exclamation point - for example email.downcase

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730034857) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+ActiveRecord::Schema.define(version: 20_170_730_034_857) do
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
-
 end
