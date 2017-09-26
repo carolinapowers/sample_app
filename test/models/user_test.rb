f81f116c733bb78ae265c23bb6370d1331f1be88
carolinapowers <carolinapoloni@gmail.com>
2017-09-26 12:56:07 -0400

    Add integration tests to signup form

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com', 
+    @user = User.new(name: 'Example User', email: 'user@example.com',
                      password: 'foobar', password_confirmation: 'foobar')
   end
 
@@ -63,12 +63,12 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'password should be present (nonblank)' do
-    @user.password = @user.password_confirmation = " " * 6
+    @user.password = @user.password_confirmation = ' ' * 6
     assert_not @user.valid?
   end
 
   test 'password should have a minimim length' do
-    @user.password = @user.password_confirmation = "a" * 5
+    @user.password = @user.password_confirmation = 'a' * 5
     assert_not @user.valid?
   end
 end
